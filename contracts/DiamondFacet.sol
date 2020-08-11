53a0ac0a5b0ecfe3605ff5f757afcf4b40956314
Nick Mudge <nick@perfectabstractions.com>
2020-08-11 11:07:10 -0400

    Code and gas improvement to DiamondFacet.sol

--- contracts/DiamondFacet.sol
+++ contracts/DiamondFacet.sol
@@ -7,6 +7,7 @@ pragma experimental ABIEncoderV2;
 *
 * Implementation of Diamond facet.
 * This is gas optimized by reducing storage reads and storage writes.
+* This code is as complex as it is to reduce gas costs.
 /******************************************************************************/
 
 import "./DiamondStorageContract.sol";
@@ -24,7 +25,7 @@ contract DiamondFacet is IDiamond, DiamondStorageContract {
         uint oldSelectorSlotsIndex;
         uint oldSelectorSlotIndex;
         bytes32 oldSelectorSlot;
-        bool newSlot;
+        bool updateLastSlot;
     }
 
     function diamondCut(bytes[] memory _diamondCut) public override {
@@ -50,7 +51,7 @@ contract DiamondFacet is IDiamond, DiamondStorageContract {
             uint position = 52;
 
             // adding or replacing functions
-            if(newFacet != 0) {
+            if(newFacet != 0) {                
                 // add and replace selectors
                 for(uint selectorIndex; selectorIndex < numSelectors; selectorIndex++) {
                     bytes4 selector;
@@ -61,6 +62,7 @@ contract DiamondFacet is IDiamond, DiamondStorageContract {
                     bytes32 oldFacet = ds.facets[selector];
                     // add
                     if(oldFacet == 0) {
+                        slot.updateLastSlot = true;
                         ds.facets[selector] = newFacet | bytes32(selectorSlotLength) << 64 | bytes32(selectorSlotsLength);
                         slot.selectorSlot = slot.selectorSlot & ~(CLEAR_SELECTOR_MASK >> selectorSlotLength * 32) | bytes32(selector) >> selectorSlotLength * 32;
                         selectorSlotLength++;
@@ -69,10 +71,6 @@ contract DiamondFacet is IDiamond, DiamondStorageContract {
                             slot.selectorSlot = 0;
                             selectorSlotLength = 0;
                             selectorSlotsLength++;
-                            slot.newSlot = false;
-                        }
-                        else {
-                            slot.newSlot = true;
                         }
                     }
                     // replace
@@ -84,6 +82,7 @@ contract DiamondFacet is IDiamond, DiamondStorageContract {
             }
             // remove functions
             else {
+                slot.updateLastSlot = true;
                 for(uint selectorIndex; selectorIndex < numSelectors; selectorIndex++) {
                     bytes4 selector;
                     assembly {
@@ -92,6 +91,7 @@ contract DiamondFacet is IDiamond, DiamondStorageContract {
                     position += 4;
                     bytes32 oldFacet = ds.facets[selector];
                     require(oldFacet != 0, "Function doesn't exist. Can't remove.");
+                    // Current slot is empty so get the slot before it
                     if(slot.selectorSlot == 0) {
                         selectorSlotsLength--;
                         slot.selectorSlot = ds.selectorSlots[selectorSlotsLength];
@@ -99,23 +99,27 @@ contract DiamondFacet is IDiamond, DiamondStorageContract {
                     }
                     slot.oldSelectorSlotsIndex = uint64(uint(oldFacet));
                     slot.oldSelectorSlotIndex = uint32(uint(oldFacet >> 64));
+                    // gets the last selector in the slot
                     bytes4 lastSelector = bytes4(slot.selectorSlot << (selectorSlotLength-1) * 32);
                     if(slot.oldSelectorSlotsIndex != selectorSlotsLength) {
                         slot.oldSelectorSlot = ds.selectorSlots[slot.oldSelectorSlotsIndex];
+                        // clears the selector we are deleting and puts the last selector in its place.
                         slot.oldSelectorSlot = slot.oldSelectorSlot & ~(CLEAR_SELECTOR_MASK >> slot.oldSelectorSlotIndex * 32) | bytes32(lastSelector) >> slot.oldSelectorSlotIndex * 32;
+                        // update storage with the modified slot
                         ds.selectorSlots[slot.oldSelectorSlotsIndex] = slot.oldSelectorSlot;
                         selectorSlotLength--;
                     }
                     else {
+                        // clears the selector we are deleting and puts the last selector in its place.
                         slot.selectorSlot = slot.selectorSlot & ~(CLEAR_SELECTOR_MASK >> slot.oldSelectorSlotIndex * 32) | bytes32(lastSelector) >> slot.oldSelectorSlotIndex * 32;
-                        selectorSlotLength--;
-                        slot.newSlot = true;
+                        selectorSlotLength--;                        
                     }
                     if(selectorSlotLength == 0) {
                         delete ds.selectorSlots[selectorSlotsLength];
-                        slot.selectorSlot = 0;
+                        slot.selectorSlot = 0;                        
                     }
                     if(lastSelector != selector) {
+                        // update last selector slot position info
                         ds.facets[lastSelector] = oldFacet & CLEAR_ADDRESS_MASK | bytes20(ds.facets[lastSelector]);
                     }
                     delete ds.facets[selector];
@@ -126,7 +130,7 @@ contract DiamondFacet is IDiamond, DiamondStorageContract {
         if(newSelectorSlotsLength != slot.originalSelectorSlotsLength) {
             ds.selectorSlotsLength = newSelectorSlotsLength;
         }
-        if(slot.newSlot) {
+        if(slot.updateLastSlot && selectorSlotLength > 0) {
             ds.selectorSlots[selectorSlotsLength] = slot.selectorSlot;
         }
         emit DiamondCut(_diamondCut);
