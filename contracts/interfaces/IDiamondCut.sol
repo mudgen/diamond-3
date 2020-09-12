2af2b43feeb15976b4e74769bd0ab7f1b39cf7f2
Nick Mudge <nick@perfectabstractions.com>
2020-09-12 17:33:00 -0400

    comments

--- contracts/interfaces/IDiamondCut.sol
+++ contracts/interfaces/IDiamondCut.sol
@@ -11,16 +11,6 @@ interface IDiamondCut {
     /// @notice Add/replace/remove any number of functions and optionally execute
     ///         a function with delegatecall
     /// @param _diamondCut Contains the facet addresses and function selectors
-    /// This argument is tightly packed for gas efficiency
-    /// That means no padding with zeros.
-    /// Here is the structure of _diamondCut:
-    /// _diamondCut = [
-    ///     abi.encodePacked(facet, sel1, sel2, sel3, ...),
-    ///     abi.encodePacked(facet, sel1, sel2, sel4, ...),
-    ///     ...
-    /// ]
-    /// facet is the address of a facet
-    /// sel1, sel2, sel3 etc. are four-byte function selectors.
     /// @param _init The address of the contract or facet to execute _calldata
     /// @param _calldata A function call, including function selector and arguments
     ///                  _calldata is executed with delegatecall on _init
